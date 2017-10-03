87ce7aebe1248d6114ce580250d4c52a7a4676c5
jangerhard <jgschoepp@gmail.com>
2017-10-03 20:24:16 +0200

    Add gitignore

--- app/src/main/java/com/example/jangerhard/BitcoinWalletTracker/MainActivity.java
+++ app/src/main/java/com/example/jangerhard/BitcoinWalletTracker/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.jangerhard.BitcoinWalletTracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mRequestQueue;
+    String url = "https://blockchain.info/";
+    TextView tvAccountName;
+    TextView tvAccountBalance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bGetAccount = (Button) findViewById(R.id.bGetAccount);
+        bGetAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWalletInfo("1FfmbHfnpaZjKFvyi1okTjJJusN455paPH");
+            }
+        });
+    }
+
+    private void getWalletInfo(String hashAddress) {
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url + "rawaddr/" + hashAddress,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        tvAccountName.setText("Got response!");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tvAccountName.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        getVolleyRequestQueue().add(stringRequest);
+
+    }
+
+    public RequestQueue getVolleyRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(this);
+        }
+
+        return mRequestQueue;
     }
 }
